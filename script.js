862d6417b292a4fa1151d73d1257b33baa951c24
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2025-12-30 14:48:33 +0800

    12_30 angid

--- script.js
+++ script.js
@@ -36,3 +36,24 @@ Ta ugluu bur ugluuniihuu tsaig zaawal
 uuj baigaarai. Udriig saihan unguruuleerei. Amjilt husey`,
   },
 ];
+const menu_container = document.querySelector(".menu-container");
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+function displayMenu(menuItems) {
+  const html = menuItems
+    .map((item) => {
+      return `<div class="card">
+  <img src="${item.img}" alt="$item.ner">
+  <div class="card-content">
+    <div class="card-header">
+      <h3>${item.ner}</h3>
+      <span class="price">${item.price}</span>
+    </div>
+    <p>${item.text}</p>
+  </div>
+</div>`;
+    })
+    .join("");
+  menu_container.innerHTML = html;
+}
+displayMenu(menu);
